8fc1022291af982104787cebe2369eefc35ea26b
Mohamed  Asif S <maasifar@gmail.com>
2025-04-03 02:17:52 +0530

    front-end question chnaged

--- HandleCallback/views.py
+++ HandleCallback/views.py
@@ -146,8 +146,8 @@ class submit_to_github(APIView):
                   message=f"Runs in {extra_submission['data']['submissionDetails']['runtimeDisplay']} using {extra_submission['data']['submissionDetails']['memoryDisplay']}, beating {round(extra_submission['data']['submissionDetails'][ 'runtimePercentile'])}% in speed. - LeetRevise "
                   content=f"{extra_submission['data']['submissionDetails']['question']['title']} {extra_submission['data']['submissionDetails']['question']['content']}"
                   print(content)
-                  question_id=extra_submission['data']['submissionDetails']['question']['questionFrontendId']
-                  res=github_uploader(token=token,repo=repo,title=f"{question_id}-{values["title"]}",lang=languages[values["lang"].capitalize()],message=message,code=values["code"],branch="main",content=content)
+              
+                  res=github_uploader(token=token,repo=repo,title=f"{adding_zero(values["question_id"],4)}-{values["title"]}",lang=languages[values["lang"].capitalize()],message=message,code=values["code"],branch="main",content=content)
                   # github_uploader(token,repo,values['title'],"py","please","print('Asifars')","main")
 
                   # print(res)
