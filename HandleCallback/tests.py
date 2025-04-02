da561cd911330502db440cdd90d2ed0ecf2b3d81
Mohamed  Asif S <maasifar@gmail.com>
2025-04-03 02:14:33 +0530

    front-end question chnaged

--- HandleCallback/tests.py
+++ HandleCallback/tests.py
@@ -1,3 +1,34 @@
 from django.test import TestCase
 
 # Create your tests here.
+query="""
+            query submissionDetail($submissionId: Int!) {
+  submissionDetails(submissionId: $submissionId) {
+    id
+    statusCode
+    runtime
+    memory
+    runtimeDisplay
+    memoryDisplay
+    timestamp
+    code
+ 
+    runtimePercentile
+    memoryPercentile
+    question {
+       questionFrontendId
+      title
+      titleSlug
+      content
+    }
+  }
+}
+
+payload={
+                "query":query,
+                "variables":{"submissionId":values['id']}
+                
+                
+            }
+        
+            extra_submission=fetch_data(url=url_details,headers=headers,payload=payload,type="post")
